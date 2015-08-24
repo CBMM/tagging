e4aa7b7d0de7ea750490f4912c0f8fb67e7e604d
Greg Hale <imalsogreg@gmail.com>
2015-08-24 00:57:27 -0400

    prelim client work

--- tagging-client/exec/SimplePics.hs
+++ tagging-client/exec/SimplePics.hs
@@ -2,12 +2,9 @@ module Main where
 
 import Reflex
 import Reflex.Dom
-import Database.Groundhog
-import Database.Groundhog.Postgresql
-import Database.Groundhog.Core
 import Tagging.Stimulus
 import Tagging.User
 import Tagging.Experiments.SimplePics.Widgets
 
 main :: IO ()
-main = mainWidget (pageWidget (TaggingUser 0 Nothing Nothing (Just (StimSeqItemKey (PersistInt64 0))) [Subject]))
+main = mainWidget (pageWidget (TaggingUser 0 Nothing Nothing (Just 0) [Subject]))
--- tagging-client/src/Crud.hs
+++ tagging-client/src/Crud.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RecursiveDo       #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Tagging.Crud where
 
 import Reflex.Dom
@@ -9,4 +12,28 @@ class (FromJSON v, ToJSON v,
       A.FromJSON (Key v BackendSpecific)
       ) => Crud v where
 
+  inputWidget :: MonadWidget t m => Proxy v -> m (Dynamic t v) -- TODO: Proxy is needed?
 
+  getEntity :: MonadWidget t m => Key v BackendSpecific -> m (Event v)
+
+  getAllEntities :: MonadWidget t m => m (Event [v])
+
+  postEntity :: MonadWidget t m => v -> m (Event ())
+
+  putEntity :: MonadWidget t m => Key v BackendSpecific -> v -> m (Event ())
+
+  deleteEntity :: MonadWidget t m => Key v BackendSpeficic -> m (Event ())
+
+
+crudTableWidget :: (MonadWidget t m, Crud v)
+  => Proxy v
+  -> Dynamic t (v -> Bool)
+  -> m (Dynamic t [v])
+crudTableWidget dynElems validate = mdo
+
+  submitButton <- button "Add"
+  newEntityDyn <- inputWidget
+  isOk         <- combineDyn validate newEntityDyn
+  
+  vs           <- holdDyn [] =<< (getAllEntities)
+  elClass "table" "crud-table" $ listViewWithKey vs crudRowWidget
