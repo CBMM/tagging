e73c6aa08dbddf33a3d7bebbbf2920702efc3dfc
Greg Hale <imalsogreg@gmail.com>
2015-08-25 23:48:55 -0400

    adminpanel tricky

--- tagging-client/src/Tagging/Crud.hs
+++ tagging-client/src/Tagging/Crud.hs
@@ -11,6 +11,8 @@ module Tagging.Crud where
 
 import           Data.Functor
 import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BS
 import           Data.Foldable
 import           Data.Map (Map(..))
 import qualified Data.Map as Map
@@ -48,61 +50,78 @@ class (A.FromJSON v, A.ToJSON v) => Crud v where
   deleteEntity :: MonadWidget t m => Proxy v -> Event t Int64 -> m (Event t ())
 
 
+-----------------------------------------------------------------------------
 crudTableWidget :: forall t m v.(MonadWidget t m, Crud v)
   => Proxy v
   -> Dynamic t (v -> Bool)
-  -> m (Dynamic t (Map Int64 v))
+  -> m (Event t (Map Int64 (CrudRowCmds v)))
 crudTableWidget p dynValidate = mdo
-
-  vMap <- holdDyn mempty =<< getAllEntities p (crudEvents p rowEvents)
+  updateEvents <- crudEvents p rowEvents
+  vMap <- holdDyn mempty =<< getAllEntities p (() <$ updateEvents)
 
   rowEvents <- elClass "table" "crud-table" $ do
     --newRowEvents      <- newRowWidget p
-    existingRowEvents <- list vMap crudRowWidget
+    existingRowEvents <- listViewWithKey vMap crudRowWidget
     --return (newRowEvents <> existingRowEvents)
     return existingRowEvents
 
-  return vMap
+  return rowEvents
 
+-----------------------------------------------------------------------------
 crudEvents :: MonadWidget t m
            => Proxy v
-           -> CrudRowEvents t m v
-           -> m (Event t ())
+           -- -> CrudRowEvents t m v
+           -> Event t (CrudRowCmds v)
+           -> m (Event t (CrudRowCmds v))
            -- -> m (Event t (Map Int64 v -> Map Int64 v)) -- this is harder!
            --   Think I need Doug's library to associate requests with
            --   responses
-crudEvents p es = do
-  postResps <- performRequestAsync $ ffor (postEvent es) $ \e ->
+crudEvents p es = ffor es $ \c -> do
+  postResps <- case crudPost c of
+    Nothing -> never
+    Just v  -> performRequestAsync $
                  XhrRequest "POST" ("/api/" <> resourceName p)
-                 def {_xhrRequestConfig_sendData = Just (A.encode e)}
-  putResps  <- performRequestAsync $ ffor (putEvent es) $ \(k,e) ->
-                 XhrRequest "PUT" ("/api/" <> resourceName p <> "/" <> show k)
-                 def {_xhrRequestConfig_sendData = Just (A.encode e)}
-  delResps  <- performRequestAsync $ ffor (delEvent es) $ \k ->
+                 def {_xhrRequestConfig_sendData = Just (BS.unpack $ A.encode v)}
+  putResps  <- case crudPut c of
+    Nothing    -> never
+    Just (k,v) -> performRequestAsync $
+                    XhrRequest "PUT" ("/api/" <> resourceName p <> "/" <> show k)
+                    def {_xhrRequestConfig_sendData = Just (BS.unpack $ A.encode v)}
+  delResps  <- case crudDel c of
+    Nothing -> never
+    Just k  -> performRequestAsync $ ffor (crudDel c) $ \k ->
                  XhrRequest "DELETE" ("/api" <> resourceName p <> "/" show k)
                  def
-  return $ (() <$ postResps)
-           (() <$ putResps)
-           (() <$ delResps)
 
-data CrudRowEvents t (m :: * -> *) v =
-  CrudRowEvents { delEvent  :: Event t Int64
-                , postEvent :: Event t v
-                , putEvent  :: Event t (Int64,v)
-}
+  return es
+  --return $ leftmost [(() <$ postResps)
+  --                  ,(() <$ putResps)
+  --                  ,(() <$ delResps)
 
-
-instance Monoid (CrudRowEvents t m v) where
-  mempty        = CrudRowEvents mempty mempty mempty
-  a `mappend` b = CrudRowEvents
-                    (delEvent  a <> delEvent  b)
-                    (postEvent a <> postEvent b)
-                    (putEvent  a <> putEvent  b)
-
+data CrudRowCmds v = CrudRowCmds {
+    crudDel  :: Maybe Int64
+  , crudPost :: Maybe v
+  , crudPut  :: Maybe (Int64, v)
+  }
+--
+-- data CrudRowEvents t (m :: * -> *) v =
+--   CrudRowEvents { delEvent  :: Event t Int64
+--                 , postEvent :: Event t v
+--                 , putEvent  :: Event t (Int64,v)
+-- }
+--
+--
+-- instance Monoid (CrudRowEvents t m v) where
+--   mempty        = CrudRowEvents mempty mempty mempty
+--   a `mappend` b = CrudRowEvents
+--                     (delEvent  a `mappend` delEvent  b)
+--                     (postEvent a `mappend` postEvent b)
+--                     (putEvent  a `mappend` putEvent  b)
+--
 
 crudRowWidget :: (MonadWidget t m, Crud v)
-              => (Int64 -> Dynamic t v -> m (Event t (CrudRowEvents t m v)))
-              -> m (Event t (CrudRowEvents t m v))
+              => (Int64 -> Dynamic t v -> m (CrudRowCmds v))
+              -> m (Event t (CrudRowCmds v))
 crudRowWidget dynIntV = do
   text "RowWidget" >> return undefined
 
@@ -111,12 +130,15 @@ instance Crud StimulusResource where
   resourceName _ = "StimulusResource"
   inputWidget  _ = do
     text "F1:"
-    f1 <- fmap T.pack $ _textInput_value =<< textInput def
+    f1 <- _textInput_value <$> textInput def
     text "F2:"
-    f2 <- fmap T.pack $ _textInput_value =<< textInput def
+    f2 <- _textInput_value <$> textInput def
     text "F3:"
-    f3 <- fmap T.pack $ _textInput_value =<< textInput def
-    return $ $( qDyn [| StimulusResource $(unqDyn [|f1|]) $(unqDyn [|f2|]) $(unqDyn [|f3|]) |] )
+    f3 <- _textInput_value <$> textInput def
+    return $ $( qDyn [| StimulusResource
+                        (T.pack $(unqDyn [|f1|]))
+                        (T.pack $(unqDyn [|f2|]))
+                        (T.pack $(unqDyn [|f3|])) |] )
 
 
 instance Crud TaggingUser where
