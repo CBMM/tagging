c080cff4125ff147015da0a6f75d175fe6f8bab4
Greg Hale <imalsogreg@gmail.com>
2015-09-01 16:17:49 -0400

    ryans fix allows initializing text fields

--- tagging-client/src/Tagging/Crud.hs
+++ tagging-client/src/Tagging/Crud.hs
@@ -37,7 +37,7 @@ class (A.FromJSON v, A.ToJSON v) => Crud v where
 
   resourceName :: Proxy v -> String
 
-  resourceWidget :: MonadWidget t m  => v -> Bool -> m (Dynamic t v)
+  resourceWidget :: MonadWidget t m  => (Dynamic t v) -> (Dynamic t Bool) -> m (Dynamic t v)
   inputWidget :: MonadWidget t m => v -> m (Dynamic t v)
   outputWidget :: MonadWidget t m => Dynamic t v ->  m ()
 
@@ -52,46 +52,33 @@ class (A.FromJSON v, A.ToJSON v) => Crud v where
 
   postEntity :: MonadWidget t m => Event t v ->  m (Event t Int64)
 
-  putEntity :: MonadWidget t m => Event t (Int64,v) -> m (Event t ())
+  putEntity :: MonadWidget t m => Proxy v -> Event t (Int64,v) -> m (Event t ())
+  putEntity p kvEvent = do
+    let req = ffor kvEvent (\(k,v) ->
+                XhrRequest "PUT"
+                ("api/" <> resourceName p <> "/" <> show k <> "/")
+                (def { _xhrRequestConfig_headers  = s "Content-Type" =: s "application/json"
+                     , _xhrRequestConfig_sendData = Just (BL.unpack $ A.encode v)}))
+    fmap (() <$) $ performRequestAsync req
 
   deleteEntity :: MonadWidget t m => Proxy v -> Event t Int64 -> m (Event t ())
 
 
 
 -----------------------------------------------------------------------------
-crudTableWidget :: forall t m v.(MonadWidget t m, Crud v)
+crudTableWidget :: forall t m v.(MonadWidget t m, Crud v, Show v)
   => Proxy v
   -> Dynamic t (v -> Bool)
   -> m (Event t ())
 crudTableWidget p dynValidate = mdo
   pl <- getPostBuild
   let xhrGetEvents = leftmost [() <$ pl] -- , () <$ tableEvents]
-  --let updateEvents = leftmost ((id <$ pl): fmap Map.elems tableEvents)
   vMapServer <- (fmap . fmap) const $ getAllEntities p xhrGetEvents
-  --let updateEvents = () <$ tableEvents
   vMap <- foldDyn ($) mempty (tableEvents <> vMapServer)
   tableEvents <- elClass "table" "crud-table" $ do
+--   eventsMap <- listViewWithKey (raceDyn "vMap: " vMap) crudRowWidget
     eventsMap <- listViewWithKey vMap crudRowWidget
-    -- Turn the row events (Event t (Map k MapEndo)) into (Event t MapEndo)
-    return $ fmap (flip (foldr ($)) .  Map.elems) eventsMap
-    --dynText =<< mapDyn (show . length) vMap
-    --forDyn vMap (\vs -> text (show $ length vs))
-    --return never
-    --newRowEvents      <- newRowWidget p
-
-    --existingRowsEvents <- listViewWithKey vMap crudRowWidget
-    -- existingRowEvents <- forDyn vMap $ \vs -> do
-    --                        let rs <- map crudRowWidget' (Map.toList vs)  :: m [Event t ()]
-    --                        return rs
-    --existingRowsEvents <- forDyn vMap $ (\vs -> crudRowWidget' (head $ Map.toList vs))
-    --return $ updated existingRowsEvents
-    --vList <- mapDyn Map.toList vMap :: MonadWidget t m => Dynamic t [(Int64,v)]
-    --vRows <- mapDyn (mapM crudRowWidget) vList
-
-    --let existingRowsEvents = leftmost vRows
-    --existingRowsEvents <- updated <$> mapDyn (mapM crudRowWidget . Map.toList) vMap
-    --return (newRowEvents <> existingRowEvents)
-    --return existingRowsEvents
+    return $ fmap (flip (foldr ($)) . Map.elems) eventsMap
   return $ () <$ tableEvents
 
 
@@ -103,24 +90,17 @@ crudRowWidget :: forall t m v.(MonadWidget t m, Crud v)
 crudRowWidget k dynVal = do
   el "tr" $ mdo
     text (show k)
-
-    --editSaveToggle <- widgetHold showWidget [ editWidget <$ editButton
-    --                                        , showWidget <$ saveButton
-    --                                        ]
     pb <- getPostBuild
-    let vPostBuild = tagDyn dynVal pb
-
+    let v0 = tagDyn dynM pb
+    vH <- headE v0
     dynEditing   <- holdDyn False $ leftmost [False <$ saveClicks,  True <$ editButton]
 
-    dynM <- undefined
---    dynM <- dyn
+    dynM <- resourceWidget dynVal dynEditing
 
-    -- widgetHold (resourceWidget vPostBuild False) $ leftmost
-    --          [resourceWidget vPostBuild False <$ saveClicks
-    --          ,resourceWidget vPostBuild True  <$ editButton]
-
-    saveAttrs <- forDyn dynEditing $ \b -> s "style" =: s ("display:" <> bool "none" "normal" b)
-    editAttrs <- forDyn dynEditing $ \b -> s "style" =: s ("display:" <> bool "normal" "none" b)
+    saveAttrs <- forDyn dynEditing $ \b ->
+      s "style" =: s ("display:" <> bool "none" "normal" b)
+    editAttrs <- forDyn dynEditing $ \b ->
+      s "style" =: s ("display:" <> bool "normal" "none" b)
 
     saveButton <- fmap fst $ elDynAttr' "button" saveAttrs $ text "Save"
     let saveClicks = (domEvent Click saveButton)
@@ -130,58 +110,50 @@ crudRowWidget k dynVal = do
     editButton <- fmap (domEvent Click . fst) $ elDynAttr' "button" editAttrs $ text "Edit"
     delButton  <- el "td" $ button "Delete"
     return $ leftmost [(Map.delete k) <$ delButton
-                      --,fAtClick
+                      ,fAtClick
                       ]
-
-editingWidget :: (MonadWidget t m, Crud v)
-              => Int64
-              -> v
-              -> m (Event t (Map Int64 v -> Map Int64 v))
-editingWidget k v0 = do
-  dynV <- resourceWidget v0 True
-  cancelClick <- el "tr" $ button "Cancel"
-  saveClick   <- el "tr" $ button "Save"
-  let vAtClick = tagDyn dynV saveClick -- :: Event t v
-  let saveFun = fmap (\v' -> Map.insert k v') vAtClick
-  return $ leftmost [ id <$ cancelClick
-                    , saveFun
-                    ]
 
 
 -----------------------------------------------------------------------------
 instance Crud StimulusResource where
   resourceName _ = "StimulusResource"
-  resourceWidget v0 b = do
-    let attrs = if b then mempty else (s "disabled" =: s "true")
+  resourceWidget dynVal dynB = do
+    pb <- getPostBuild
+    let pbV = (tag (current dynVal) pb)
+    attrs <- forDyn dynB $ \b ->
+                if b then mempty else (s "disabled" =: s "disabled")
     f1 <- el "td" $ do
           _textInput_value <$> textInput
-            --(def { _textInputConfig_setValue = fmap (T.unpack . srName) v0 })
-            (def { _textInputConfig_initialValue = T.unpack . srName $ v0})
-          --  def { _textInputConfig_setValue = fmap (T.unpack . srName) v0
-          --      , _textInputConfig_attributes   = constDyn attrs}
+            (TextInputConfig { _textInputConfig_setValue =
+                                 fmap (\v -> T.unpack . srName $ v) pbV
+                             , _textInputConfig_attributes   = attrs
+                             , _textInputConfig_inputType = "text"
+                             , _textInputConfig_initialValue = "empty"})
     f2 <- el "td" $ do
           _textInput_value <$> textInput
-            def -- {_textInputConfig_setValue = fmap (T.unpack . srUrlSuffix) v0}
-          --      , _textInputConfig_attributes  = constDyn attrs}
+            (TextInputConfig { _textInputConfig_setValue =
+                                 fmap (\v -> T.unpack . srUrlSuffix $ v) pbV
+                             , _textInputConfig_attributes   = attrs
+                             , _textInputConfig_inputType = "text"
+                             , _textInputConfig_initialValue = "empty"})
     f3 <- el "td" $ do
           _textInput_value <$> textInput
-            def -- { _textInputConfig_setValue = fmap (T.unpack . srMimeType) v0}
-          --      , _textInputConfig_attributes   = constDyn attrs}
+            (TextInputConfig { _textInputConfig_setValue =
+                                 fmap (\v -> T.unpack . srMimeType $ v) pbV
+                             , _textInputConfig_attributes   = attrs
+                             , _textInputConfig_inputType = "text"
+                             , _textInputConfig_initialValue = "empty"})
     $( qDyn [| StimulusResource
                         (T.pack $(unqDyn [|f1|]))
                         (T.pack $(unqDyn [|f2|]))
                         (T.pack $(unqDyn [|f3|])) |] )
-  -- inputWidget = flip resourceWidget False
-  -- outputWidget dynVal = do
-  --   el "td" $ forDyn dynVal (T.unpack . srName)
-  --   el "td" $ forDyn dynVal (T.unpack . srUrlSuffix)
-  --   el "td" $ forDyn dynVal (T.unpack . srMimeType)
-  --   return ()
-
 
 instance Crud TaggingUser where
   resourceName _ = "TaggingUser"
   resourceWidget  _ = undefined
+instance Eq StimulusResource where
+  StimulusResource a b c == StimulusResource d e f =
+    a == d && b == e && c ==f
 
 s :: String -> String
 s = id
