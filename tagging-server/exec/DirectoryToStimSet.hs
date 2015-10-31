9469774d744cbd08dc5f24fbe3f627da3a079698
Greg Hale <imalsogreg@gmail.com>
2015-10-31 10:06:14 -0400

    more work on stimset

--- tagging-server/exec/DirectoryToStimSet.hs
+++ tagging-server/exec/DirectoryToStimSet.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE QuasiQuotes     #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -11,9 +12,29 @@ import Database.Groundhog.Postgresql
 import Options.Applicative
 import Data.String.QQ
 import System.Directory
+import System.FilePath
 import System.IO
+import Prelude hiding (FilePath)
 
 import Tagging.Stimulus
+
+------------------------------------------------------------------------------
+-- | The main point of the utility: grab video files from dir and
+--   build a StimulusSequence from them
+work :: DirOpts -> IO StimulusSequence
+work opt@DirOpts{..} = do
+
+  files <- filterM doesFileExist =<< getDirectoryContents directory
+
+  let fileGroups = groupBy takeBasename . sort $ map takeFilename files
+
+  stimSeqItems <- concat <$> traverse (getFilesByType opt) vidExtensions
+
+  let stimSeq = StimulusSequence title stimSeqItems descr urlBase
+  BS.writeFile outFile (A.encode stimSeq)
+
+getFilesByType :: DirOpts -> FilePath -> IO [StimSeqItem]
+getFilesByType DirOpts{..} ext = 
 
 data SortBy = Name | Created | Modified
   deriving (Eq, Show, Read, Enum, Bounded)
@@ -25,10 +46,9 @@ data DirOpts = DirOpts
   { directory :: !FilePath
   , sortBy    :: !SortBy
   , urlBase   :: !T.Text
+  , outFile   :: !FilePath
   , title     :: !T.Text
   , descr     :: !T.Text
-  , host      :: !String
-  , passwd    :: !String
   } deriving (Show)
 
 fullDescription :: String
@@ -38,45 +58,32 @@ Using a local directory as a reference, insert a StimulusSequence
 entry with metadata provided on the command-line, and links to the
 files found in the base directory.
 
-Videos are sorted into subdirectory by extension:
-  ${baseDir}/ogg/file.ogg, ${baseDir}/mp4/file.mp4
+Videos are grouped by basename:
+  [ [clip1.mov, clip1.ogg], [clip2.mov, clip2.ogg] ... ]
 
 The file part of the filename (no path, no extension), is concatenated
 with an s3 secret key, if one is provided, and the result is hmac-sha256
 encoded
-
 |]
 
--- dirOpts :: Parser DirOpts
--- dirOpts = DirOpts
---   <$> (option auto (long "path" <> short 'p'
---                    <> help "Path to data directory") <|> pure ".")
---   <*> (option auto (long "sort" <> short 's'
---                    <> help ("Sort by [" ++ sr ++ "]")) <|> pure Name)
---   <*> option auto (long "url" <> short 'u' <> help "Base url")
---   <*> option auto (long "title" <> short 't' <> help "Title")
---   <*> option auto (long "description" <> short 'd' <> help "Description")
---   <*> (strOption (long "host" <> short 'h' <> help "Database host")
---       <|> pure "localhost")
---   <*> strOption   (long "pass" <> help "Database password")
---   where sr = intercalate "|" (map show [minBound..(maxBound :: SortBy)])
+dirOpts :: Parser DirOpts
+dirOpts = DirOpts
+  <$> (option auto (long "path" <> short 'p'
+                   <> help "Path to data directory") <|> pure ".")
+  <*> (option auto (long "sort" <> short 's'
+                   <> help ("Sort by [" ++ sr ++ "]")) <|> pure Name)
+  <*> option auto (long "url" <> short 'u' <> help "Base url")
+  <*> option auto (long "title" <> short 't' <> help "Title")
+  <*> option auto (long "description" <> short 'd' <> help "Description")
+  <*> (option auto (long "output" <> short 'o' <> help "Output json file")
+       <|> pure "./out.json")
+  where sr = intercalate "|" (map show [minBound..(maxBound :: SortBy)])
 
--- fullOpts = info (helper <*> dirOpts)
---            (fullDesc
---            <> progDesc fullDescription
---            <> header "directoryToStimSet - a tool for Tagging")
+fullOpts = info (helper <*> dirOpts)
+           (fullDesc
+           <> progDesc fullDescription
+           <> header "directoryToStimSet - a tool for Tagging")
 
--- main :: IO ()
--- main = do
---   DirOpts{..} <- execParser fullOpts
---   files <- (\\ [".",".."]) <$> getDirectoryContents directory
---   let dbStr = unwords [ "host=" ++ host
---                       , "port=5432"
---                       , "dbname=tagging user=tagging"
---                       , "password=" ++ passwd
---                       ]
---   withPostgresqlConn dbStr $ runDbConn $ do
---     let stimSeq = StimulusSequence title Nothing descr urlBase
---     existingKey <- either id id <$> insertBy SsName stimSeq
---     undefined
-main = undefined
+
+main :: IO ()
+main = execParser fullOpts >>= work
