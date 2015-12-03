c1bc9f679273733c0728eca411e6a53a91e5db8a
Greg Hale <imalsogreg@gmail.com>
2015-12-03 00:07:31 -0500

    Add setup scripts

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+KEY=$1
+URL=$2
+
+scp -r -i $KEY tagging-server/static $URL:tagging-server/
+scp -i $KEY tagging-server/dist/build/tagging-server-tagging-server $URL:tagging-server/
+scp -r -i $KEY tagging-server/snaplets $URL:tagging-server/
