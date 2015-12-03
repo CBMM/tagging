ba1b2fa3c684d647d4115328f433c6136f81557f
Greg Hale <imalsogreg@gmail.com>
2015-12-03 02:08:59 -0500

    Updates for UI labels

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ KEY=$1
 URL=$2
 
 scp -r -i $KEY tagging-server/static $URL:tagging-server/
-scp -i $KEY tagging-server/dist/build/tagging-server-tagging-server $URL:tagging-server/
+scp -i $KEY tagging-server/dist/build/tagging-server/tagging-server $URL:tagging-server/
 scp -r -i $KEY tagging-server/snaplets $URL:tagging-server/
