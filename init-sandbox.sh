f278417f2fffd79cb4abca5070a9965ea6d6f767
Greg Hale <imalsogreg@gmail.com>
2015-12-15 11:04:50 -0500

    deps bump

--- init-sandbox.sh
+++ init-sandbox.sh
@@ -8,7 +8,7 @@ cabal sandbox add-source deps/servant-snap/deps/snap/deps/io-streams
 cabal sandbox add-source deps/servant-snap/deps/snap/deps/io-streams-haproxy
 cabal sandbox add-source deps/servant-snap/deps/snap/deps/snap-core
 cabal sandbox add-source deps/servant-snap/deps/snap/deps/snap-server
---cabal sandbox add-source deps/servant-snap/deps/snap/deps/xmlhtml
+cabal sandbox add-source deps/servant-snap/deps/snap/deps/xmlhtml
 cabal sandbox add-source deps/servant-snap/deps/snap/deps/heist
 cabal sandbox add-source deps/servant-snap/deps/snap
 cabal sandbox add-source deps/snap-loader-dynamic
