8b305ef6cb1ff914d5a515023d33c4d6ab7c56e4
Greg Hale <imalsogreg@gmail.com>
2016-03-25 22:54:05 -0400

    check in first parts of video widget

--- videomemory/src/Main.hs
+++ videomemory/src/Main.hs
@@ -5,35 +5,60 @@
 
 module Main where
 
-import Control.Monad (mzero)
-import Data.Aeson
-import qualified Data.Aeson as A
-import Data.Bool
+import           Control.Lens
+import           Control.Monad              (mzero)
+import           Data.Aeson
+import qualified Data.Aeson                 as A
+import           Data.Bool
 import qualified Data.ByteString.Lazy.Char8 as BSL
-import Data.Foldable
-import Data.Monoid
-import qualified Data.Text as T
-import Data.Traversable (forM)
-import GHC.Generics
-import Reflex
-import Reflex.Dom
-import Tagging.Response
-import Tagging.Stimulus
-import Tagging.User
+import           Data.Char                  (toLower)
+import           Data.Either
+import           Data.Foldable
+import           Data.Monoid
+import qualified Data.Text                  as T
+import           Data.Traversable           (forM)
+import           GHC.Generics
+import           MediaElement
+import           Reflex
+import           Reflex.Dom
+import           Reflex.Dom.Contrib.Widgets.Common
+import           Reflex.Dom.Contrib.Widgets.ButtonGroup
+import           Tagging.Response
+import           Tagging.Stimulus
+import           Tagging.User
 
-mimeOf :: String -> String
-mimeOf fn = case extension fn of
-  "oog" -> "video/oog"
-  "mp4" -> "video/mp4"
-  where extension = reverse . takeWhile (/= '.') . reverse
 
-data StimResponse = StimResponse
-  { _respRemember :: Bool }
-  deriving (Eq, Ord, Show, Generic)
+------------------------------------------------------------------------------
+data ClipResponse = ClipResponse
+  { _crRemember :: Bool
+  } deriving (Eq, Ord, Show, Read, Generic)
 
-instance ToJSON StimResponse
-instance FromJSON StimResponse
+instance ToJSON   ClipResponse
+instance FromJSON ClipResponse
 
+
+------------------------------------------------------------------------------
+data Survey = Survey
+  { _sName                :: String
+  , _sHasSeenTestEpisode  :: Bool
+  , _sHasSeenMoreEpisodes :: Bool
+  , _sVideoTest           :: (String,String)
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON   Survey
+instance FromJSON Survey
+
+
+------------------------------------------------------------------------------
+data Response = RClip   ClipResponse
+              | RSurvey Survey
+  deriving (Eq, Show, Generic)
+
+instance ToJSON   Response
+instance FromJSON Response
+
+
+------------------------------------------------------------------------------
 run :: forall t m.MonadWidget t m => m ()
 run = mdo
   pb <- getPostBuild
@@ -42,12 +67,15 @@ run = mdo
   posTry <- getAndDecode ("/api/fullposinfo" <$ stimRequestTriggers)
 
   let pos :: Event t (Assignment, StimulusSequence, StimSeqItem) = fmapMaybe id posTry
+
   errorText (text "Failed to load position")
             (True <$ ffilter (== Nothing) posTry)
-  responses :: Dynamic t (Event t StimResponse) <- elClass "div" "interaction" $
+
+  responses :: Dynamic t (Event t Response) <- elClass "div" "interaction" $
     widgetHold (text "Waiting..." >> return never) (fmap videoQuestion pos)
+
   submitSuccess <- performRequestAsync
-    (ffor (switchPromptlyDyn responses) $ \(r :: StimResponse) ->
+    (ffor (switchPromptlyDyn responses) $ \(r :: Response) ->
       XhrRequest "POST" "/api/response?advance" $
       XhrRequestConfig ("Content-Type" =: "application/json")
       Nothing Nothing Nothing (Just . BSL.unpack $ A.encode
@@ -56,9 +84,10 @@ run = mdo
   return ()
 
 
+------------------------------------------------------------------------------
 videoQuestion :: forall t m.MonadWidget t m
               => (Assignment, StimulusSequence, StimSeqItem)
-              -> m (Event t StimResponse)
+              -> m (Event t Response)
 videoQuestion (asgn, stimseq, ssi) = do
   elClass "div" "videoandquestion" $ do
     vid <- elAttr' "video" ("height"   =: "320"
@@ -76,8 +105,10 @@ videoQuestion (asgn, stimseq, ssi) = do
        ns <- fmap (False <$) (button "No")
        return $ leftmost [ys,ns]
     remembered <- holdDyn Nothing $ fmap Just rememb
-    return $ fmap StimResponse $ fmapMaybe id (updated remembered)
+    return $ fmap (RClip . ClipResponse) $ fmapMaybe id (updated remembered)
 
+
+------------------------------------------------------------------------------
 welcome :: MonadWidget t m => m (Event t ())
 welcome = elClass "div" "welcome" $ do
   text $ unwords ["Thank you for participating in this experiment."
@@ -89,12 +120,119 @@ welcome = elClass "div" "welcome" $ do
   text $ "Logging in later will bring you to your last spot in the experiment."
   button "Continue"
 
-main :: IO ()
-main = mainWidget run
 
+------------------------------------------------------------------------------
+-- | An externally-prodded validating label & text-input
+bootstrapLabeledInput :: forall t m a.MonadWidget t m
+                      => String
+                      -> String
+                      -> (String -> Either String a)
+                      -> Event t ()
+                      -> m (Dynamic t (Either String a))
+bootstrapLabeledInput label idattr validate eval =
+  elClass "div" "input-group" $ mdo
+    elAttr "label" ("for" =: idattr) $ text label
+    attrs <- forDyn isOk $ \ok ->
+      "type"  =: "text" <>
+      "id"    =: idattr <>
+      "class" =: bool "form-control invalid" "form-control valid" ok
+    mapDyn (bool mempty ("class" =: "valid")) isOk
+
+    v <- mapDyn validate =<<
+         (fmap value $ textInput (def & attributes .~ attrs))
+    isOk :: Dynamic t Bool <- mapDyn isRight v
+    return v
+
+
+------------------------------------------------------------------------------
+-- | An externally-prodded validating label & radio-button group
+radioMultichoice :: (MonadWidget t m, Eq a, Ord a, Show a)
+                 => String
+                 -> String
+                 -> [(a, String)]
+                 -> (Maybe a -> Either String a)
+                 -> Event t ()
+                 -> m (Dynamic t (Either String a))
+radioMultichoice label idattr choices validate eval = mdo
+  grpAttr <- holdDyn True (tag (fmap isRight $ current r) eval) >>= \isOk ->
+    forDyn isOk (\b -> "class" =:
+                       bool "radio-group invalid" "radio-group valid" b)
+  r <- elDynAttr "div" grpAttr $ mdo
+    elAttr "label" ("for" =: idattr) (text label)
+    rMaybe <- value <$> radioGroup
+         (constDyn idattr)
+         (constDyn choices)
+         (def & widgetConfig_attributes .~ constDyn ("id" =: idattr))
+    mapDyn validate rMaybe
+  return r
+
+
+------------------------------------------------------------------------------
+-- | Survey questions, emits a Survey when form is filled out right
+survey :: MonadWidget t m => m (Event t Survey)
+survey = mdo
+  name  <- bootstrapLabeledInput "Name" "user-real-name" validateName sends
+  seen1 <- radioMultichoice
+           "Have you seen 24, Season 5 Episode 1?"
+           "seen1" [(True,"Yes"),(False,"No")] validateSeenEp1 sends
+  seenN <- radioMultichoice
+           "Have you seen any other episodes of 24?"
+           "seenN" [(True,"Yes"),(False,"No")] validateSeenEpN sends
+  sends <- button "Ok"
+  undefined
+  where
+    validateName "" = Left "Please enter your full name"
+    validateName n  = Right n
+    validateSeenEp1 (Just False) = Left
+      "This test is only for subjects who saw 24 Season 5, Episode 1"
+    validateSeenEp1 Nothing      = Left
+      "Please indicate whether or not you saw Episode 1"
+    validateSeenEp1 (Just True)  = Right True
+    validateSeenEpN (Just False)  = Left
+      "This test is for subjects who have only seen 24 Season 5 Episode 1"
+    validateSeenEpN Nothing      = Left
+      "Please indicate whether or not you saw any other episodes of 24"
+    validateSeenEpN (Just True) = Right True
+    validateVidLook   n
+      | map toLower n == "brain" = Right "brain"
+      | otherwise                = Left
+        "Sorry, that isn't the work shown in the video"
+    validateVidListen n
+      | map toLower n == "mind" = Right "mind"
+      | otherwise               = Left
+        "Sorry, that isn't the word spoken in the video"
+
+
+
+------------------------------------------------------------------------------
+main' :: IO ()
+main' = mainWidget run
+
+main :: IO ()
+main = mainWidget $ do
+  play <- button "play"
+  pause <- button "pause"
+  muted <- toggle False =<< button "Toggle mute"
+  el "br" (return ())
+  w <- videoWidget [("file:///home/greghale/Downloads/test4.mp4","video/mp4")]
+       (def & (videoWidgetConfig_play .~ play)
+            . (videoWidgetConfig_pause .~ pause)
+            . (videoWidgetConfig_setMuted .~ updated muted))
+  el "br" (return ())
+  display =<< count (w ^. videoWidget_ended)
+  text "test"
+  display (w ^. videoWidget_currentTime)
+
+  return ()
+
+
+------------------------------------------------------------------------------
+-- | Convenience
 fin :: MonadWidget t m => m ()
 fin = return ()
 
+
+------------------------------------------------------------------------------
 -- | Throwaway error-reporting widget
 errorText :: MonadWidget t m => m () -> Event t Bool -> m ()
 errorText msg setShowing = mdo
@@ -106,4 +244,15 @@ errorText msg setShowing = mdo
     (e,_) <- elAttr' "span" ("class" =: "glyphicon glyphicon-remove") fin
     return $ domEvent Click e
   fin
+
+
+------------------------------------------------------------------------------
+-- | Mime type string for various video file extensions
+mimeOf :: String -> String
+mimeOf fn = case extension fn of
+  "ogg" -> "video/ogg"
+  "mp4" -> "video/mp4"
+  "webm" -> "video/webm"
+  where extension = reverse . takeWhile (/= '.') . reverse
+
 
---
+++ videomemory/src/MediaElement.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- Specification:
+-- https://www.w3.org/TR/html5/embedded-content-0.html#htmlmediaelement
+-- https://www.w3.org/TR/html5/embedded-content-0.html#mediaevents
+
+module MediaElement where
+
+import Control.Lens
+import Data.Default
+import Data.Foldable
+import Data.Traversable
+import Data.Map
+import Data.Monoid
+import GHCJS.DOM.EventM
+import qualified GHCJS.DOM.HTMLMediaElement as Media
+import Reflex
+import Reflex.Dom
+
+------------------------------------------------------------------------------
+-- | Try to replicate as much of the functionality at
+--   https://www.w3.org/2010/05/video/mediaevents.html as possible
+data VideoWidgetConfig t = VideoWidgetConfig
+  { _videoWidgetConfig_attributes :: Dynamic t (Map String String)
+  , _videoWidgetConfig_sourceAttributes :: (String,String) -> Dynamic t (Map String String)
+  , _videoWidgetConfig_load :: Event t ()
+  , _videoWidgetConfig_play :: Event t ()
+  , _videoWidgetConfig_pause :: Event t ()
+  , _videoWidgetConfig_setCurrentTime :: Event t Double
+  , _videoWidgetConfig_setPlaybackRate :: Event t Double
+  , _videoWidgetConfig_setVolume :: Event t Double
+  , _videoWidgetConfig_setMuted :: Event t Bool
+  }
+
+makeLenses ''VideoWidgetConfig
+
+instance Reflex t => Default (VideoWidgetConfig t) where
+  def = VideoWidgetConfig defAttr f never never never never never never never
+    where defAttr = constDyn mempty
+          f (srcUrl, srcMime) = constDyn ("src" =: srcUrl <> "type" =: srcMime)
+
+data VideoWidget t = VideoWidget
+  { _videoWidget_videoEl :: El t
+  , _videoWidget_sourceEls :: [El t]
+  , _videoWidget_currentTime :: Dynamic t Double
+  , _videoWidget_playbackRate :: Dynamic t Double
+  , _videoWidget_volume :: Dynamic t Double
+  , _videoWidget_muted :: Dynamic t Bool
+  , _videoWidget_loadstart :: Event t ()
+  , _videoWidget_canplaythrough :: Event t ()
+  , _videoWidget_ended :: Event t ()
+  }
+
+makeLenses ''VideoWidget
+
+------------------------------------------------------------------------------
+-- | Create a video playing widget with access to video controls, events, and
+--   state
+videoWidget :: MonadWidget t m
+            => [(String, String)] -- ^ Pairs of source url and mime-type
+            -> VideoWidgetConfig t
+            -> m (VideoWidget t)
+videoWidget srcs cfg = do
+  (vidEl, vidSrcs) <- elDynAttr' "video" (_videoWidgetConfig_attributes cfg) $
+    forM srcs $ \src ->
+      fmap fst $ elDynAttr' "source"
+                            (_videoWidgetConfig_sourceAttributes cfg src)
+                            (return ())
+
+  let e = Media.castToHTMLMediaElement (_el_element vidEl)
+
+  -- Property getters
+  -- TODO: What are the correct initial values?
+  curTime <- holdDyn 0 =<<
+             wrapDomEvent e (`on` Media.timeUpdate) (Media.getCurrentTime e)
+  vidEnded <- wrapDomEvent e (`on` Media.ended) (return ())
+  vidMuted <- holdDyn False =<<
+              wrapDomEvent e (`on` Media.volumeChange) (Media.getMuted e)
+  vidVolume <- holdDyn 1 =<<
+               wrapDomEvent e (`on` Media.volumeChange) (Media.getVolume e)
+  -- Pushers & Setters
+  performEvent_ $ Media.load e <$ _videoWidgetConfig_load cfg
+  performEvent_ $ Media.play e <$ _videoWidgetConfig_play cfg
+  performEvent_ $ Media.pause e <$ _videoWidgetConfig_pause cfg
+  performEvent_ $ Media.setCurrentTime e
+                  <$> _videoWidgetConfig_setCurrentTime cfg
+  performEvent_ $ Media.setPlaybackRate e
+                  <$> _videoWidgetConfig_setPlaybackRate cfg
+  performEvent_ $ Media.setVolume e
+                  <$> _videoWidgetConfig_setVolume cfg
+  performEvent_ $ Media.setMuted e
+                  <$> _videoWidgetConfig_setMuted cfg
+
+
+  return $ VideoWidget vidEl vidSrcs curTime undefined vidVolume vidMuted undefined undefined vidEnded
+
